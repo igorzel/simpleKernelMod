c04a2635bbb647d89e4317cb7ccb2c070a81c4a6
svarozhych <svarozhych@svarozhych-pc.(none)>
2013-01-24 00:46:02 +0200

    Added support of /proc file system

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ obj-m := algo_kernel_mod.o
 KDIR := /usr/src/linux-headers-$(shell uname -r)
 PWD := $(shell pwd)
 
-all:
+default:
 	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) modules
 
 clean:
-	rm -rf *.mod.c *.o *.ko *.order *.symvers
+	rm -rf *.mod.c *.o *.ko *.order *.symvers .*.cmd
--- algo_kernel_mod.c
+++ algo_kernel_mod.c
@@ -9,20 +9,87 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/proc_fs.h>
 
-MODULE_LICENSE("GPL");
 
-static int algo_kernel_mod_init(void)
+/******************************************************************************/
+/*                                                                            */
+/******************************************************************************/
+#define PROC_FILE_NAME ("algo_kernel_mod")
+
+/******************************************************************************/
+/*                                                                            */
+/* Викликається при читанні з /proc файла пристрою                            */
+/*                                                                            */
+/******************************************************************************/
+static int
+algo_kernel_mod_read_proc(char *buffer, char **start, off_t offset, int size, int *eof,
+                void *data)
+{
+    const char* defaultData = "Data from algo_kernel_mod\n";
+	const int dataSize = strlen(defaultData);
+
+	// дозволяємо читатати лише з початку файлу
+	if (offset != 0)
+	{
+		return 0;
+	}
+
+	// перевіряємо чи достатнього розміру буфер нам передали
+	if (size < dataSize)
+	{
+		return EINVAL;
+	}
+
+	// копіюємо дані у буфер
+	strcpy(buffer, defaultData);
+
+	// виставляємо ознаку кінця файлу
+	*eof = 1;
+
+	return dataSize;
+}
+
+/******************************************************************************/
+/*                                                                            */
+/* Функція викликається при початковій ініціалізації модуля                   */
+/*                                                                            */
+/******************************************************************************/
+static int __init algo_kernel_mod_init(void)
 {
 	printk(KERN_DEBUG "Hello from algo_kernel_mod!\n");
+
+	if (create_proc_read_entry(PROC_FILE_NAME, 0, NULL, algo_kernel_mod_read_proc, NULL) == 0)
+	{
+		printk(KERN_ERR "Unable to register \"%s\" proc file\n", PROC_FILE_NAME);
+
+		return ENOMEM;
+	}
+
 	return 0;
 }
 
-static void algo_kernel_mod_exit(void)
+/******************************************************************************/
+/*                                                                            */
+/* Функція викликається при закінченні роботи модуля                          */
+/*                                                                            */
+/******************************************************************************/
+static void __exit algo_kernel_mod_exit(void)
 {
 	printk(KERN_DEBUG "Good bye to algo_kernel_mod!\n");
+
+	remove_proc_entry(PROC_FILE_NAME, NULL);
 }
+
+/******************************************************************************/
 
 module_init(algo_kernel_mod_init);
 module_exit(algo_kernel_mod_exit);
+
+/******************************************************************************/
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("svarozhych");
+MODULE_DESCRIPTION("A sample, linux kernel module.");
+MODULE_VERSION("1.0");
 
